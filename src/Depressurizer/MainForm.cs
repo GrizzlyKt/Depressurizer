a45897a6ed3b296ca8ea6a653a51e40c7133cabd
Martijn Vegter <mail@mvegter.me>
2018-08-23 17:11:50 +0200

    Added MainForm and DotSettings

---
+++ src/Depressurizer/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace Depressurizer
+{
+    public partial class MainForm : MaterialForm
+    {
+        #region Constructors and Destructors
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeMaterialSkin();
+        }
+
+        #endregion
+
+        #region Properties
+
+        private static MaterialSkinManager MaterialSkinManager => MaterialSkinManager.Instance;
+
+        #endregion
+
+        #region Methods
+
+        private void InitializeMaterialSkin()
+        {
+            MaterialSkinManager.AddFormToManage(this);
+            MaterialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            MaterialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
+
+        #endregion
+    }
+}
--- src/Depressurizer/Program.cs
+++ src/Depressurizer/Program.cs
@@ -1,22 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Depressurizer
 {
-    static class Program
+    internal static class Program
     {
+        #region Methods
+
         /// <summary>
-        /// The main entry point for the application.
+        ///     The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
+
+        #endregion
     }
 }
